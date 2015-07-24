f41be82a15b7887794457f0619116dde695dfe90
Rania Khoury <rania.kh3107@gmail.com>
2015-07-24 18:14:50 +0300

    css plugis

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,47 @@
+/**
+ * JS Library v0
+ */
+
+var UTILS = (function () {
+
+    return {
+        /**
+         * Check if a given value is a plain Object
+         *
+         * @param  {*}       o Any value to be checked
+         * @return {Boolean}   true if it's an Object
+         */
+        isObject: function (o) {
+            var toString = Object.prototype.toString;
+            return (toString.call(o) === toString.call({}));
+        },
+
+        addEvent: function ( obj, type, handler ) {
+            if ( obj.attachEvent ) {
+            obj['e'+type+handler] = handler;
+            obj[type+handler] = function(){
+                obj['e'+type+handler]( window.event );
+                };
+            obj.attachEvent( 'on'+type, obj[type+handler] );
+            } else
+            obj.addEventListener( type, handler, false );
+        },
+
+        removeEvent: function( obj, type, handler ) {
+            if ( obj.detachEvent ) {
+                obj.detachEvent( 'on'+type, obj[type+handler] );
+                obj[type+handler] = null;
+            } else
+            obj.removeEventListener( type, handler, false );
+        },
+    
+    };
+}());
+
+
+
+
+
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
